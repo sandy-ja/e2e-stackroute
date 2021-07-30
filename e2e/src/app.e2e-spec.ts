a73c442db4e1941c74cb56a415c37177b9079c3d
Jarada <sandy.jarada@cgi.com>
2021-07-30 08:17:47 -0400

    done

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,29 +9,51 @@ describe('Movie App', () => {
   });
 
   it('should load movie component on base url', () => {
+    browser.get('/');
+    element(by.linkText('Add Movie')).click();
+    expect(browser.getCurrentUrl())
+      .toEqual(browser.baseUrl + 'addmovie');
     
   });
 
   it('should load movie component view on clicking add movie and verify the url', () => {
+    browser.get('addmovie');
+    element(by.linkText('Add Movie')).click();
+    expect(browser.getCurrentUrl())
+      .toEqual(browser.baseUrl + 'addmovie');
    
   });
 
   it('should contain <router-outlet>', () => {
+    page.navigateTo();
+    expect(page.getRouterOutlet).toBeTruthy('<router-outlet> should exist in app.component.html');
    
   });
 
   // given empty title to form
   it('should check form if title is empty', async () => {
+    page.navigateToAddMovie();
+    element(by.css('input[type="text"]')).sendKeys();
+    element(by.css('.btn')).click();
+    expect(element(by.css('.alert')).getText()).toBe('Title and Story should not be empty!!! Please verify details');
    
   });
 
   // given empty story to form
   it('should check form if story is empty', async () => {
+    page.navigateToAddMovie();
+    element(by.css('textarea')).sendKeys();
+    element(by.css('.btn')).click();
+    expect(element(by.css('.alert')).getText()).toBe('Title and Story should not be empty!!! Please verify details');
    
   });
 
   // given valid title to form and empty story
   it('should check form is valid or not', async () => {
+    page.navigateToAddMovie();
+    element(by.css('input[type="text"]')).sendKeys('test');
+    element(by.css('.btn')).click();
+    expect(element(by.css('.alert')).getText()).toBe('Title and Story should not be empty!!! Please verify details');
    
   });
 
